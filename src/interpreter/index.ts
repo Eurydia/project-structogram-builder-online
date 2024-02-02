872c5a48a5a6f833d35a9d4e8ae2b467c7bc22f1
Euryn <crossdeltavi@gmail.com>
2024-02-02 17:55:32 +0100

    Update index.ts

--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -1,2 +1,9 @@
+/**
+ * Exposes the lexer and parser to the rest of the application.
+ *
+ * The "lexerInit" and "lexerGetAllTokens" functions create a lexer and tokenize an input string.
+ *
+ * The "parserInit" and "parserGetAllNodes" function create a parser and build an abstract syntax tree.
+ */
 export * from "./lexer";
 export * from "./parser";
